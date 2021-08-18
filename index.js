509eca2db70c9333ebd1e6825732ab7525f30f9c
ricky <ricky.chaggar@gmail.com>
2021-08-18 11:23:47 +0100

    set up async function to take user text input and submit a get request via fetch to funtranslations. Translated text is then displayed on screen

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 "use strict";
 
-console.log("Script is running");
+const inputText = document.querySelector("#text");
+const outputText = document.querySelector("#translation");
+const form = document.querySelector("#form");
+
+const url = "https://api.funtranslations.com/translate/pirate.json?text=";
+
+form.addEventListener("submit", onSubmit);
+
+async function onSubmit(e) {
+  let toTranslate = encodeURIComponent(inputText.value.trim());
+  e.preventDefault();
+  try {
+    let response = await fetch(url + toTranslate);
+    let results = await response.json();
+    outputText.value = results.contents.translated;
+    console.log(results.contents.translated);
+  } catch (err) {
+    console.log(err);
+  }
+}
