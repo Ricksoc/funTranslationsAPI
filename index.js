6a146ec369f0c8a862ca22cd74b8d446df43a8f1
ricky <ricky.chaggar@gmail.com>
2021-08-18 12:28:32 +0100

    added error handler for when a language is not selected

--- index.js
+++ index.js
@@ -3,20 +3,28 @@
 const inputText = document.querySelector("#text");
 const outputText = document.querySelector("#translation");
 const form = document.querySelector("#form");
+const languageSelect = document.querySelector("#language-select");
 
-const url = "https://api.funtranslations.com/translate/pirate.json?text=";
+const url = "https://api.funtranslations.com/translate/";
+const urlJoin = ".json?text=";
 
 form.addEventListener("submit", onSubmit);
 
 async function onSubmit(e) {
+  outputText.value = "";
   let toTranslate = encodeURIComponent(inputText.value.trim());
+  let language = languageSelect.value;
   e.preventDefault();
   try {
-    let response = await fetch(url + toTranslate);
+    let response = await fetch(url + language + urlJoin + toTranslate);
     let results = await response.json();
     outputText.value = results.contents.translated;
     console.log(results.contents.translated);
   } catch (err) {
-    console.log(err);
+    if (language === "") {
+      outputText.value = "You did not select a language";
+    } else {
+      console.log(err);
+    }
   }
 }
